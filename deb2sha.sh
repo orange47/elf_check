bbd531921eff1c2b2851314dbb6bf8012b1e8dc9
orange47 <orange47@gmail.com>
2018-05-09 20:42:25 +0200

    Add files via upload

---
+++ deb2sha.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#elf -> sha
+
+echo "usage: deb2sha.sh inputdir/ outputdir/"
+
+if [[ $# -eq 0 ]] ; then
+    echo 'no args'
+    exit 0
+fi
+
+tempdir='/mnt/ramdisk/1'
+aptdir="$1"
+date=`date +%Y-%m-%d`
+output="$2$date"
+mkdir "$output"
+
+IFS=$'\n'
+
+
+function sumit {
+ workdir="$1"
+ outputfile="$output/$2.sha3"
+
+ for fdeb in $(find $workdir -mmin -300 -name '*.deb' -type f)
+ do
+  fakeroot dpkg -x "$fdeb" "$tempdir"
+
+  for f in $(find "$tempdir" -name '*' -type f)
+  do
+   if file "$f" | grep -q ": ELF " 
+   then
+    fname=`basename "$f"`
+    flen=`stat -c %s "$f"`
+    sha=`rhash --sha3-512 "$f" | head -c 128`
+    echo "$sha,$flen,$fname" >> "$output/$outputfile"
+   fi
+  done
+ 
+  rm -rf "$tempdir"
+ 
+ done
+
+}  
+
+
+
+
+
+
+for x in {{0..9},{a..z},lib{0..9},lib{a..z}}
+do
+      echo "$x"
+    for d in $(find "$aptdir" -regextype egrep  -regex ".*\/(contrib|main|non-free)\/$x\/.*"  -type d)
+    do
+     sumit "$d" "$x"
+    done
+    
+done
---
+++ elfchk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "usage: elfchk.sh dbfile  inputdir/ "
+
+if [[ $# -eq 0 ]] ; then
+    echo 'no args'
+    exit 0
+fi
+
+dbfile="$1"
+elfdir="$2"
+
+IFS=$'\n'
+
+
+  for f in $(find "$elfdir" -type f)
+  do
+   if file -b -e apptype -e ascii -e encoding -e tokens -e cdf -e compress -e elf -e tar -e text "$f" | grep -i -q ^elf 
+   then
+    fname=`basename "$f"`
+    flen=`stat -c %s "$f"`
+    shack="`rhash --sha3-512 "$f" | head -c 128`,$flen,$fname"
+    if ! grep -F -q "$shack" "$dbfile" 
+    then
+     echo "ERR:$shack"
+    fi
+   fi
+  done
+ 
+
+ echo "DONE"
+exit 0
+
+
+
+# real    61m14.593s bez -e
+
+
+#  -e apptype -e ascii -e encoding -e tokens -e cdf -e compress -e elf -e soft -e tar -e text    
